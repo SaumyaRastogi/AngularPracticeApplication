7ff8ac06c9cb8b1b8d3e24323b30c4cdedae602d
Saumya Rastogi <saumyarastogi1729@gmail.com>
2023-04-27 11:56:54 +0530

    SharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterUserFormComponent } from './SharedModule/Register/register-user-form/register-user-form.component';
+import { HeaderComponent } from './SharedModule/Header/header/header.component';
+import { LoginFormComponent } from './SharedModule/Login/login-form/login-form.component';
+import { HomePageComponent } from './SharedModule/Home/home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, 
+    AppComponent,
+    RegisterUserFormComponent,
+    HeaderComponent,
+    LoginFormComponent,
+    HomePageComponent, 
   ],
   imports: [
     BrowserModule,
