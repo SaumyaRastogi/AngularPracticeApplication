c15a1b54cb5fab786f13606b61e4749e082b30a2
vishnupriyach1712 <vishnupriyach.vp@gmail.com>
2023-04-28 11:01:00 +0530

    reg form

--- src/app/SharedModule/Register/register-user-form/register-user-form.component.ts
+++ src/app/SharedModule/Register/register-user-form/register-user-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register-user-form',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-user-form.component.css']
 })
 export class RegisterUserFormComponent implements OnInit {
+  registrationForm!: FormGroup;
+  skills: string[] = ['HTML', 'CSS', 'JavaScript', 'Angular', 'React', 'Vue'];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      role: ['', Validators.required],
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      contact: ['', Validators.required],
+      skills: this.buildSkills(),
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
   }
 
+  buildSkills() {
+    const skillsArr = this.skills.map(skill => {
+      return this.fb.control(false);
+    });
+    return this.fb.array(skillsArr);
+  }
+
+  passwordMatchValidator(frm: FormGroup) {
+    return frm.controls['password'].value === frm.controls['confirmPassword'].value ? null : {'mismatch': true};
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+      // You can send the registration
+    }
+
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginFormComponent } from './SharedModule/Login/login-form/login-form.component';
+import { RegisterUserFormComponent } from './SharedModule/Register/register-user-form/register-user-form.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginFormComponent
+  },
+  {
+    path: 'register',
+    component: RegisterUserFormComponent
   }
 ];
 
